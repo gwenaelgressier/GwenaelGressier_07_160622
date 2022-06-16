831933318d4263d77be8b616b6d6f8ce9f3f642e
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-06-16 23:25:15 +0200

    ajout auth

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 require(`dotenv`).config({ path: `./config/.env` });
 require("./config/db");
 const userRoutes = require("./routes/user.routes");
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routes
 app.use("/api/user", userRoutes);
